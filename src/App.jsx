cdc8c5eda8b41b9eb350bb0908b064a057b5aa7e
meddebadem000@gmail.com <meddebadem000@gmail.com>
2024-12-20 18:25:23 +0100

    CorrectSomeIssues

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import MyFooter from './components/MyFooter';
 import { useLocation } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
+import Navbar from './components/Navbar';
 
 function App() {
   const { token } = useAuth();
