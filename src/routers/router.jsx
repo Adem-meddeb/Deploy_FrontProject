61522701b4a6cf9aa8065c9bddff4b1d8459854e
meddebadem000@gmail.com <meddebadem000@gmail.com>
2024-12-20 18:34:37 +0100

    CorrectSomeIssues

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import {
   createBrowserRouter,
-  RouterProvider,
 } from "react-router-dom";
 import App from "../App";
 import Home from "../home/Home";
 import Login from '../components/Login';
 import Register from "../components/Registration";
 import ProtectedRoute from "../components/ProtectedRoute";
-import { AuthProvider } from "../context/AuthContext"; 
 import Profile from '../components/Profile';
 import EventComponent from '../components/EventComponent';
 import EditEventComponent from '../components/EditEventComponent';
